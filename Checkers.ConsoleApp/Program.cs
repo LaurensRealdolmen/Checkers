987b487102e5b81e048451b86163145b4cd505fd
Laurens Herman <laurens.herman@realdolmen.com>
2018-10-19 15:32:00 +0200

    Add console app + Fix reference to dapper

---
+++ Checkers.ConsoleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Checkers.Game;
+
+namespace Checkers.ConsoleApp
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var gameState = new GameState();
+            Console.WriteLine($"Possible moves: {gameState.GetAllMoves()}.");
+            Console.WriteLine($"Game state overview: {gameState}.");
+            Console.ReadKey();
+        }
+    }
+}
