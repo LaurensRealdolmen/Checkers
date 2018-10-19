4c2f50402f1fd7ffe9a0119596ed42935554a2d3
Evert De Vos <Evert.DeVos@realdolmen.com>
2018-10-19 14:57:09 +0200

    implemented state repository

--- Checkers.DataAccess.Dapper/StateRepository.cs
+++ Checkers.DataAccess.Dapper/StateRepository.cs
@@ -1,10 +1,35 @@
-﻿using System;
+﻿using Checkers.Game;
+using Dapper;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Text;
 
 namespace Checkers.DataAccess.Dapper
 {
-    class StateRepository
+    public class StateRepository
     {
+        private const string connectionString = @"Server=(localdb)\cherckers;Integrated Security=true;";
+
+        public IEnumerable<string> GetStates()
+        {
+            var sql = @"SELECT Value FROM States";
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+                return connection.Query<string>(sql).AsList() ;
+            }
+        }
+
+        public void SaveState( int id, string state)
+        {
+            var sql = "INSERT INTO States (Value) Values (@Value)";
+            
+            using (var connection =  new SqlConnection(connectionString))
+            {
+                connection.Open();
+                connection.Execute(sql, state);
+            }
+        }
     }
 }
